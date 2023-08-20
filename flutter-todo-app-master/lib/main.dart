1cb24f7995a1fb73b408b69b8ee7d16ea43b2be8
MonirNoyon <monirpstu1@gmail.com>
2023-08-20 22:00:18 +0600

    Firebase integration done

--- flutter-todo-app-master/lib/main.dart
+++ flutter-todo-app-master/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './screens/home.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,11 +16,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ToDo App',
-      home: Home(),
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ToDo App',
+        home: Home(),
+      ),
     );
   }
 }
--- flutter-todo-app-master/lib/widgets/todo_item.dart
+++ flutter-todo-app-master/lib/widgets/todo_item.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 import '../model/todo.dart';
 import '../constants/colors.dart';
+import '../model/todo_model.dart';
 
 class ToDoItem extends StatelessWidget {
-  final ToDo todo;
-  final onToDoChanged;
-  final onDeleteItem;
+  final TodoModelData todo;
+  Function() onToDoChanged;
+  Function() onDeleteItem;
 
-  const ToDoItem({
+   ToDoItem({
     Key? key,
     required this.todo,
     required this.onToDoChanged,
@@ -18,27 +19,24 @@ class ToDoItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(bottom: 20),
+      margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {
-          // print('Clicked on Todo Item.');
-          onToDoChanged(todo);
-        },
+        onTap: onToDoChanged,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
-          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          todo.isChecked ? Icons.check_box : Icons.check_box_outline_blank,
           color: tdBlue,
         ),
         title: Text(
-          todo.todoText!,
+          todo.title,
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
-            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+            decoration: todo.isChecked ? TextDecoration.lineThrough : null,
           ),
         ),
         trailing: Container(
@@ -53,11 +51,8 @@ class ToDoItem extends StatelessWidget {
           child: IconButton(
             color: Colors.white,
             iconSize: 18,
-            icon: Icon(Icons.delete),
-            onPressed: () {
-              // print('Clicked on delete icon');
-              onDeleteItem(todo.id);
-            },
+            icon: const Icon(Icons.delete),
+            onPressed: onDeleteItem,
           ),
         ),
       ),
